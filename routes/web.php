0e1aa0f1333a0fe67701b3dd8ae02ba875475fec
Syun199986 <syun990806@gmail.com>
2023-06-07 15:02:19 +0900

    AdminLTEの追加

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('adminlte', function () {
+    return view('adminlte');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
